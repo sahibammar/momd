cdc61bade727d79a5a93eb0c65ee70cf1140ad7d
sahibammar <sahib.ammar@gmail.com>
2021-06-18 15:42:29 +0200

    removing example tables

--- project/sqlserver/commands.sql
+++ project/sqlserver/commands.sql
@@ -4,31 +4,6 @@ GO
 USE momd
 GO
 
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE momd..department (
-	department_id INT PRIMARY KEY IDENTITY (1, 1),
-	department_name  VARCHAR (50) NOT NULL
-)
-GO
-
-CREATE TABLE momd..employee (
-    employee_id INT PRIMARY KEY IDENTITY (1, 1),
-    first_name VARCHAR (50) NOT NULL,
-    last_name VARCHAR (50) NOT NULL,
-    phone VARCHAR(20),
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES momd..department (department_id)
-)
-GO
-
 CREATE TABLE momd..MohafathaLookup (
     MohafathaCode INT PRIMARY KEY,
     MohafathaName VARCHAR(50)
